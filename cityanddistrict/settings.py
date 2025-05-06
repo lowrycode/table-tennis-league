5a1312a86d7fc8e587bdd7ed6a8fdc358cd1f439
Pete Lowry <peter.m.lowry@gmail.com>
2025-05-06 22:41:59 +0100

    Add database and prepare for deployment

--- cityanddistrict/settings.py
+++ cityanddistrict/settings.py
@@ -1,4 +1,9 @@
 from pathlib import Path
+import os
+import sys
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,10 +13,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wh=jeqwgvoqj^k$*i*3kxvj#-l$l77h75h&ah4afa3+nrl6c-4'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG_MODE", "False") == "True"
 
 ALLOWED_HOSTS = []
 
@@ -58,16 +63,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'cityanddistrict.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+if 'test' in sys.argv:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 
 # Password validation
