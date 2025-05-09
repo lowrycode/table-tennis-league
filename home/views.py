155cda20872de539202e2a85683f53c09b9a5993
Pete Lowry <peter.m.lowry@gmail.com>
2025-05-10 00:12:50 +0100

    Update view logic to fetch and display active news items in the template

--- home/views.py
+++ home/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import NewsItem
+from django.utils import timezone
+from django.db import models
 
 
 def home(request):
-    return render(request, 'home/home.html')
+    now = timezone.now()
+    active_news_items = NewsItem.objects.filter(active_from__lte=now).filter(
+        models.Q(active_to__isnull=True) | models.Q(active_to__gte=now)
+    )
+    return render(request, "home/home.html", {"active_news_items": active_news_items})
