5326265ca79a32c71623f0fd660f4ca5fe1c3ca6
Pete Lowry <peter.m.lowry@gmail.com>
2025-05-09 22:35:11 +0100

    Add property and custom validation to NewsItem model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.utils import timezone
 from django.db import models
 
@@ -10,7 +11,26 @@ class NewsItem(models.Model):
     active_to = models.DateTimeField(null=True, blank=True)
 
     class Meta:
-        ordering = ['-active_from', 'title']
+        ordering = ["-active_from", "title"]
 
     def __str__(self):
         return self.title
+
+    @property
+    def is_active(self):
+        now = timezone.now()
+        return self.active_from <= now and (
+            self.active_to is None or self.active_to >= now
+        )
+
+    def clean(self):
+        super().clean()
+        # Check active_to is later than active_from
+        if self.active_to and self.active_from >= self.active_to:
+            raise ValidationError(
+                '"Active from" date must be earlier than "Active to" date.'
+            )
+        # Check active_to is in future
+        now = timezone.now()
+        if self.active_to and self.active_to <= now:
+            raise ValidationError('"Active to" date must be in the future.')
