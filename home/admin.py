6ded82c6e261f1cfde3dad7b835a2ea3b282a671
Pete Lowry <peter.m.lowry@gmail.com>
2025-05-09 17:38:01 +0100

    Customise admin panel to include filter and specify fields for NewsItem model

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import NewsItem
 
-admin.site.register(NewsItem)
+
+@admin.register(NewsItem)
+class NewsItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'active_from', 'active_to')
+    list_filter = ('active_from', 'active_to')
