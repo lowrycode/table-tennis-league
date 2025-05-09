855a3cfa76c314e726271e8b274e1119f6314cd4
Pete Lowry <peter.m.lowry@gmail.com>
2025-05-09 10:46:34 +0100

    Add FAQ section to homepage

--- home/tests/test_views.py
+++ home/tests/test_views.py
@@ -4,25 +4,25 @@ from django.urls import reverse
 
 class HomePageTests(TestCase):
     def test_homepage_returns_correct_status_code(self):
-        response = self.client.get(reverse("home"))
+        response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
 
     def test_homepage_returns_correct_template(self):
-        response = self.client.get(reverse("home"))
+        response = self.client.get(reverse('home'))
         self.assertTemplateUsed(response, "home/home.html")
 
     def test_homepage_has_hidden_title(self):
-        response = self.client.get(reverse("home"))
+        response = self.client.get(reverse('home'))
         self.assertContains(response, '<h1 class="visually-hidden"')
-        self.assertContains(response, 'City and District Table Tennis League')
+        self.assertContains(response, "City and District Table Tennis League")
 
     # Hero Section
     def test_homepage_contains_hero_section(self):
-        response = self.client.get(reverse("home"))
+        response = self.client.get(reverse('home'))
         self.assertContains(response, '<section id="hero"')
 
     def test_homepage_contains_hero_image(self):
-        response = self.client.get(reverse("home"))
+        response = self.client.get(reverse('home'))
         self.assertContains(response, "<picture>")
         self.assertContains(response, 'srcset="/static/images/hero.webp"')
         self.assertContains(response, 'srcset="/static/images/hero.jpg"')
@@ -32,7 +32,7 @@ class HomePageTests(TestCase):
         )
 
     def test_homepage_contains_hero_overlay(self):
-        response = self.client.get(reverse("home"))
+        response = self.client.get(reverse('home'))
         self.assertContains(response, 'id="hero-overlay"')
         self.assertContains(response, 'src="/static/images/hero-overlay.png"')
         self.assertContains(
@@ -42,9 +42,21 @@ class HomePageTests(TestCase):
 
     # About Section
     def test_homepage_contains_about_section(self):
-        response = self.client.get(reverse("home"))
+        response = self.client.get(reverse('home'))
         self.assertContains(response, '<section id="about"')
         self.assertContains(
-            response, "Welcome to the City and District Table Tennis League!"
+            response, 'Welcome to the City and District Table Tennis League!'
         )
-        self.assertContains(response, "Founded in 2015")
+        self.assertContains(response, 'Founded in 2015')
+
+    # FAQ Section
+    def test_homepage_contains_faq_section(self):
+        response = self.client.get(reverse('home'))
+        self.assertContains(response, '<section id="faq"')
+        self.assertContains(
+            response,
+            'Find answers to the most frequently asked questions',
+        )
+        self.assertContains(response, 'id="accordion-faq"')
+        self.assertContains(response, 'class="accordion-item')
+        self.assertContains(response, 'Who can join the league?')
