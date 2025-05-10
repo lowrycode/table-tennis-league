5b09b63ceeab818de7113d3569c9cbcb993247c1
Pete Lowry <peter.m.lowry@gmail.com>
2025-05-10 21:04:16 +0100

    Register Enquiry model in admin panel

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Enquiry
 
-# Register your models here.
+
+@admin.register(Enquiry)
+class EnquiryAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'name', 'submitted_at', 'is_actioned')
+    list_filter = ('is_actioned',)
