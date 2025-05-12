97d3723e2633e9e228a044e9b27e843254ab8bef
Pete Lowry <peter.m.lowry@gmail.com>
2025-05-12 15:57:46 +0100

    "Add django-allauth package and configure settings"

--- cityanddistrict/urls.py
+++ cityanddistrict/urls.py
@@ -3,8 +3,9 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
-    path('', include("home.urls"), name="home-urls"),
     path('clubs/', include("clubs.urls"), name="clubs-urls"),
     path('contact/', include("contact.urls"), name="contact-urls"),
+    path('', include("home.urls"), name="home-urls"),
 ]
