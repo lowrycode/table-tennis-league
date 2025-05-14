2966d6c54cd577f7946100a201bfdb050a2e5280
Pete Lowry <peter.m.lowry@gmail.com>
2025-05-14 22:43:24 +0100

    URL paths refactored to give clearer hierarchy and confirm_account_delete page added

--- useraccounts/urls.py
+++ useraccounts/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('account-settings/', views.account_settings, name="account_settings"),
-    path('change-email/', views.change_email, name="change_email"),
+    path("account/settings/", views.account_settings, name="account_settings"),
+    path("account/email/change/", views.change_email, name="change_email"),
+    path(
+        "account/delete/confirm/", views.delete_account, name="delete_account"
+    ),
 ]
