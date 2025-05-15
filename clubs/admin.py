cc80ceee972a51f26aa2e0aec82abc355d30242a
Pete Lowry <peter.m.lowry@gmail.com>
2025-05-15 17:58:54 +0100

    Register Club model in Django Admin panel

--- clubs/admin.py
+++ clubs/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import ClubInfo
+from .models import Club, ClubInfo
 
 # Register your models here.
+admin.site.register(Club)
 admin.site.register(ClubInfo)
