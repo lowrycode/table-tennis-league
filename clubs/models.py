0d17438dff7f0cd4d48bfa6670fb405f87ff8e0e
Pete Lowry <peter.m.lowry@gmail.com>
2025-05-15 23:03:56 +0100

    Add remaining fields to ClubInfo model
    - includes 'contact_phone', 'description', 'session_info' and boolean fields

--- clubs/models.py
+++ clubs/models.py
@@ -1,5 +1,7 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 from cloudinary.models import CloudinaryField
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Club(models.Model):
@@ -17,6 +19,35 @@ class ClubInfo(models.Model):
     image = CloudinaryField("image", default="placeholder")
     contact_name = models.CharField(max_length=100)
     contact_email = models.EmailField()
+    contact_phone = PhoneNumberField(null=True, blank=True)
+    description = models.TextField()
+    session_info = models.TextField()
+    beginners = models.BooleanField(default=False)
+    intermediates = models.BooleanField(default=False)
+    advanced = models.BooleanField(default=False)
+    kids = models.BooleanField(default=False)
+    adults = models.BooleanField(default=False)
+    coaching = models.BooleanField(default=False)
+    league = models.BooleanField(default=False)
+    equipment_provided = models.BooleanField(default=False)
+    membership_required = models.BooleanField(default=False)
+    free_taster = models.BooleanField(default=False)
+    approved = models.BooleanField(default=False)
 
     def __str__(self):
         return self.club.name
+
+    def clean(self):
+        super().clean()
+        if len(self.description) > 500:
+            raise ValidationError(
+                {
+                    "description": "Cannot be more than 500 characters."
+                }
+            )
+        if len(self.session_info) > 500:
+            raise ValidationError(
+                {
+                    "session_info": "Cannot be more than 500 characters."
+                }
+            )
--- clubs/tests/test_models.py
+++ clubs/tests/test_models.py
@@ -37,6 +37,9 @@ class ClubInfoTests(TestCase):
             "website": "https://www.example.com",
             "contact_name": "Joe Bloggs",
             "contact_email": "example@example.com",
+            "contact_phone": "01234556778",
+            "description": "This club is the best!",
+            "session_info": "We do every night of the week.",
         }
         self.club_info = ClubInfo.objects.create(**self.info_data)
 
@@ -48,23 +51,54 @@ class ClubInfoTests(TestCase):
     def test_string_representation(self):
         self.assertEqual(str(self.club_info), self.club_info.club.name)
 
+    # Multi-field tests
     def test_required_fields(self):
-        club_info = ClubInfo()
-        with self.assertRaises(ValidationError) as cm:
-            club_info.full_clean()
-        errors = cm.exception.message_dict
+        required_fields = {
+            "club": True,
+            "image": False,
+            "website": False,
+            "contact_name": True,
+            "contact_email": True,
+            "contact_phone": False,
+            "description": True,
+            "session_info": True,
+            "beginners": False,
+            "intermediates": False,
+            "advanced": False,
+            "kids": False,
+            "adults": False,
+            "coaching": False,
+            "league": False,
+            "equipment_provided": False,
+            "membership_required": False,
+            "free_taster": False,
+            "approved": False,
+        }
 
-        self.assertIn("club", errors, msg="club should be required")
-        self.assertNotIn("image", errors, msg="image should not be required")
-        self.assertNotIn(
-            "website", errors, msg="website should not be required"
-        )
-        self.assertIn(
-            "contact_name", errors, msg="contact_name should be required"
-        )
-        self.assertIn(
-            "contact_email", errors, msg="contact_email should be required"
-        )
+        # Check each field
+        for field, is_required in required_fields.items():
+            helper_test_required_fields(self, field, is_required)
+
+    def test_boolean_field_defaults(self):
+        boolean_fields = {
+            "beginners": False,
+            "intermediates": False,
+            "advanced": False,
+            "kids": False,
+            "adults": False,
+            "coaching": False,
+            "league": False,
+            "equipment_provided": False,
+            "membership_required": False,
+            "free_taster": False,
+            "approved": False,
+        }
+
+        # Check each field
+        for field, default_value in boolean_fields.items():
+            helper_test_boolean_default(
+                field, default_value, self.info_data.copy()
+            )
 
     # Tests for club field
     def test_club_field_one_to_one(self):
@@ -105,6 +139,40 @@ class ClubInfoTests(TestCase):
         with self.assertRaises(ValidationError):
             self.club_info.full_clean()
 
+    # Tests for contact_phone field
+    def test_invalid_phone_number(self):
+        # Test not a number
+        self.club_info.contact_phone = "Invalid"
+        with self.assertRaises(ValidationError):
+            self.club_info.full_clean()
+
+        # Test invalid region
+        self.club_info.contact_phone = "4155552671"  # US phone number
+        with self.assertRaises(ValidationError):
+            self.club_info.full_clean()
+
+    # Tests for description field
+    def test_description_max_length(self):
+        # Check valid at threshold
+        self.club_info.description = "a" * 500
+        self.club_info.full_clean()
+
+        # Check invalid above threshold
+        self.club_info.description = "a" * 501
+        with self.assertRaises(ValidationError):
+            self.club_info.full_clean()
+
+    # Tests for session_info field
+    def test_session_info_max_length(self):
+        # Check valid at threshold
+        self.club_info.session_info = "a" * 500
+        self.club_info.full_clean()
+
+        # Check invalid above threshold
+        self.club_info.session_info = "a" * 501
+        with self.assertRaises(ValidationError):
+            self.club_info.full_clean()
+
     # Tests for image field
     def test_image_field_defaults_to_placeholder(self):
         # Create a new club for this test instance
@@ -120,3 +188,39 @@ class ClubInfoTests(TestCase):
 
         # Check placeholder is recorded as default
         self.assertEqual(club_info.image, "placeholder")
+
+
+# Helper functions
+def helper_test_boolean_default(field_name, default_value, info_data):
+    # Create a new club for this test instance
+    club = Club.objects.create(name="A Different Club Name")
+
+    # Amend info_data
+    info_data["club"] = club
+    info_data.pop(field_name, None)
+
+    # Create ClubInfo object from info_data
+    club_info = ClubInfo.objects.create(**info_data)
+
+    # Check placeholder is recorded as default
+    result = getattr(club_info, field_name) == default_value
+
+    # Tidy up and return result
+    club.delete()
+    return result
+
+
+def helper_test_required_fields(test_case, field_name, is_required):
+    club_info = ClubInfo()
+    with test_case.assertRaises(ValidationError) as cm:
+        club_info.full_clean()
+    errors = cm.exception.message_dict
+
+    if is_required:
+        test_case.assertIn(
+            field_name, errors, msg=f"{field_name} should be required"
+        )
+    else:
+        test_case.assertNotIn(
+            field_name, errors, msg=f"{field_name} should not be required"
+        )
