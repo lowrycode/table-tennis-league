498953e4c443f10c0b765dc1aa8373df347fd970
Pete Lowry <peter.m.lowry@gmail.com>
2025-05-19 16:17:31 +0100

    Add club_admin_required custom decorator

---
+++ clubs/decorators.py
@@ -0,0 +1,14 @@
+from functools import wraps
+from django.core.exceptions import PermissionDenied
+from django.contrib.auth.views import redirect_to_login
+
+
+def club_admin_required(view_func):
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect_to_login(request.get_full_path())
+        if not hasattr(request.user, 'club_admin'):
+            raise PermissionDenied
+        return view_func(request, *args, **kwargs)
+    return _wrapped_view
