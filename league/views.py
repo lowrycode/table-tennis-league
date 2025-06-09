b67eb252d728a4c2c1ccd653e36ea44904362322
Pete Lowry <peter.m.lowry@gmail.com>
2025-06-09 12:53:07 +0100

    Add Fixtures page under League section
    - Added 'League' to navbar with a dropdown showing 'Fixtures'
    - Implemented URL route, view, and template for the Fixtures page with initial styling
    - Note: Venue links still need to be implemented

--- league/views.py
+++ league/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Season, Week
 
-# Create your views here.
+
+def fixtures(request):
+    season = get_object_or_404(Season, is_current=True)
+    weeks = (
+        Week.objects.filter(season=season)
+        .prefetch_related(
+            "week_fixtures__home_team",
+            "week_fixtures__away_team",
+            "week_fixtures__venue",
+        )
+        .order_by("start_date")
+    )
+
+    return render(
+        request, "league/fixtures.html", {"season": season, "weeks": weeks}
+    )
